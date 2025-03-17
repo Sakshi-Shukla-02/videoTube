bac95e6a0dd96ccd04a331b98d69dd927eb262a7
Sakshi Shukla <sakshishukla271102@gmail.com>
2025-03-17 16:56:27 +0000

    installed packages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("OOPS! Server failed to connect", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed !!! Check index.js", error);
+  });
 
 /*import express from "express";
 const app = express();
