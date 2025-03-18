4ccd54d782fe7dc855d263e5bcc0639adce4a83e
Sakshi Shukla <sakshishukla271102@gmail.com>
2025-03-18 09:09:57 +0000

    Utils and app

--- src/app.js
+++ src/app.js
@@ -3,4 +3,16 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, //explore more
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 export { app };
